f8a73fef33d5481b86bdb7e4db9dbbb7939ca1b9
dahye <dadaJJJung@gmail.com>
2024-05-28 20:10:16 +0900

    SendMoneyCommand 테스트 작성

---
+++ src/test/java/com/example/banknig/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banknig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
