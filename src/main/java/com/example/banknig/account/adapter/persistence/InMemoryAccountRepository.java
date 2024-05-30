b486b425d0b23e8f3204a0f519f6b239ef607499
dahye <dadaJJJung@gmail.com>
2024-05-30 20:54:33 +0900

    REFACTOR: 계좌 조회시 리턴타입 Account에서 Optional<Account>로 수정

--- src/main/java/com/example/banknig/account/adapter/persistence/InMemoryAccountRepository.java
+++ src/main/java/com/example/banknig/account/adapter/persistence/InMemoryAccountRepository.java
@@ -4,11 +4,14 @@ import com.example.banknig.account.application.port.LoadAccountPort;
 import com.example.banknig.account.domain.Account;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public class InMemoryAccountRepository implements LoadAccountPort {
 
     @Override
-    public Account loadAccount(Long accountId) {
+    public Optional<Account> loadAccount(Long accountId) {
         return null;
     }
+
 }
--- src/main/java/com/example/banknig/account/application/port/LoadAccountPort.java
+++ src/main/java/com/example/banknig/account/application/port/LoadAccountPort.java
@@ -2,6 +2,8 @@ package com.example.banknig.account.application.port;
 
 import com.example.banknig.account.domain.Account;
 
+import java.util.Optional;
+
 public interface LoadAccountPort {
-    Account loadAccount(Long accountId);
+    Optional<Account> loadAccount(Long accountId);
 }
