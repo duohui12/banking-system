f92f99b11bab60b2c0f5ed49105404074abab081
dahye <dadaJJJung@gmail.com>
2024-05-30 20:56:42 +0900

    FEAT: Account 도메인 withdraw, deposit 기능 추가

--- src/main/java/com/example/banknig/account/domain/Account.java
+++ src/main/java/com/example/banknig/account/domain/Account.java
@@ -1,17 +1,29 @@
 package com.example.banknig.account.domain;
 
+
 public class Account {
 
-    //TODO: 도메인 개발
-
     private Long accountId;
-    private int balance;
+    private Long balance;
 
-    public boolean withdraw(int amount){
+
+    //출금
+    public boolean withdraw(Long amount){
+        if(!mayWithdraw(amount)){
+            return false;
+        }
+        balance -= amount;
         return true;
     }
 
-    public boolean deposit(int amount){
+    //출금가능여부 확인
+    private boolean mayWithdraw(Long amount){
+        return balance >= amount;
+    }
+
+    //입금
+    public boolean deposit(Long amount){
+        balance += amount;
         return true;
     }
 
