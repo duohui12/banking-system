8c5e4bc506b7a08d6caa8a5826c033b63e573de7
dahye <dadaJJJung@gmail.com>
2024-06-01 17:19:46 +0900

    FEAT: Account 도메인 기능 추가

--- src/main/java/com/example/banknig/account/domain/Account.java
+++ src/main/java/com/example/banknig/account/domain/Account.java
@@ -1,11 +1,18 @@
 package com.example.banknig.account.domain;
 
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE) //모든 필드를 포함한 private 생성자.
+@Getter
+@Setter
 public class Account {
 
     private Long accountId;
     private Long balance;
-
 
     //출금
     public boolean withdraw(Long amount){
@@ -25,6 +32,10 @@ public class Account {
     public boolean deposit(Long amount){
         balance += amount;
         return true;
+    }
+
+    public static Account getAccountInstance(Long accountId, Long balance){
+        return new Account(accountId, balance);
     }
 
 }
