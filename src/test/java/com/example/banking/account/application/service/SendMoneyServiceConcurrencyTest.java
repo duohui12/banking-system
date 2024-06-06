55a8a52d52cc91c7085aebacdece5cebe77f9923
dahye <dadaJJJung@gmail.com>
2024-06-06 19:59:23 +0900

    TEST: 비관적락 걸고 송금할때 데드락 걸리는 상황 테스트

--- src/test/java/com/example/banking/account/application/service/SendMoneyServiceConcurrencyTest.java
+++ src/test/java/com/example/banking/account/application/service/SendMoneyServiceConcurrencyTest.java
@@ -6,6 +6,7 @@ import com.example.banking.account.domain.Account;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.util.concurrent.CountDownLatch;
@@ -38,8 +39,8 @@ class SendMoneyServiceConcurrencyTest {
     class Describe_send_money {
 
         @Nested
-        @DisplayName("A계좌에서 B계좌로, 100번의 송금요청을 동시에 보내면")
-        class Context_with_100_concurrent_send_money_requests{
+        @DisplayName("A계좌에서 B계좌로, 동일한 금액을 100번 송금하면")
+        class Context_with_100_one_way_concurrent_requests{
 
             private SendMoneyCommand validCommand = new SendMoneyCommand(sourceAccountId
                                                                         , targetAccountId
@@ -71,6 +72,67 @@ class SendMoneyServiceConcurrencyTest {
             }
         }
 
+        @Nested
+        @DisplayName("A계좌에서 B계좌로, B계좌에서 A계좌로 동일한 금액을 100번 송금하면")
+        class Context_with_100_two_way_concurrent_requests{
+
+            //비관적락 걸었을 때 데드락 걸리는 상황 생각해보기
+
+            //초기 상태
+            //A계좌 잔액 : 5000원, B계좌 잔액 : 5000원
+            //A->B 로 50원*100번 요청 => 총 5000원 송금
+            //B->A 로 50원*100번 요청 => 총 5000원 송금
+
+            //예상했던 결과
+            //A: 5000 - (50*100) + (50*100) = 5000원 (초기 잔액 그대로)
+            //B: 5000 - (50*100) + (50*100) = 5000원 (초기 잔액 그대로)
+
+            //실제 결과
+            //1.A->B로 송금하는 트랜잭션1 실행, A에 락걸고 B 얻기 위해 대기
+            //3.B->A로 송금하는 트랜잭션2 실행, B에 락걸고 A 얻기 위해 대기
+            //트랜잭션1과 트랜잭션2가 각각 자원 선점하고 무한대기
+
+            //h2는 쿼리에 타임아웃 설정x
+
+
+            private SendMoneyCommand sourceToTargetCommand =new SendMoneyCommand(sourceAccountId
+                                                                                    , targetAccountId
+                                                                                    , sendMoneyAmount);
+
+            private SendMoneyCommand targetToSourceCommand =new SendMoneyCommand(targetAccountId
+                                                                                , sourceAccountId
+                                                                                , sendMoneyAmount);
+
+            @Test
+            @Timeout(5)
+            @DisplayName("A계좌의 잔액과, B계좌의 잔액은 그대로이다.")
+            void it_updates_balance_to_the_same_value_as_before() throws InterruptedException {
+
+                int threadCount = 100;
+                ExecutorService executorService = Executors.newFixedThreadPool(50);
+                CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+                for (int i = 1; i <= threadCount; i++) {
+                    executorService.submit(() -> {
+                        sendMoneyService.sendMoney(sourceToTargetCommand);
+                        sendMoneyService.sendMoney(targetToSourceCommand);
+                        countDownLatch.countDown();
+                    });
+                }
+
+                countDownLatch.await();
+
+                Account sourceAccount = accountPersistenceAdapter.loadAccount(sourceAccountId);
+                Account targetAccount = accountPersistenceAdapter.loadAccount(targetAccountId);
+
+                assertThat(sourceAccount.getBalance()).isEqualTo(sourceAccountBalance);
+                assertThat(targetAccount.getBalance()).isEqualTo(targetAccountBalance);
+
+            }
+        }
+
     }
 
 }
+
+
